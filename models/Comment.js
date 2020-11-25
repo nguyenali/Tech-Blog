4b1b73ca8a5aba0c4e03dd8f5c3371d5fbbfd2e5
Ali Nguyen <alin57717@gmail.com>
2020-11-24 19:29:56 -0800

    create comment model for database table

---
+++ models/Comment.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        comment_text: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            },
+            validate: {
+                len: [1]
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
+
+module.exports = Comment;
